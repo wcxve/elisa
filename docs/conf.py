790ee308609ea1886953ecd6cbcc8e2b9975225a
xuewc <wcxuemail@gmail.com>
2023-11-15 01:04:50 +0800

    update

--- docs/conf.py
+++ docs/conf.py
@@ -8,7 +8,11 @@
 
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../..'))
+import pathlib
+path = pathlib.Path(__file__)
+sys.path.insert(0, path.parent.parent.as_posix())
+print(path.parent.parent.as_posix())
+import bayespec
 
 
 project = 'BayeSpec'
