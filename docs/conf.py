1251e27ec63e68d586a9158fa7f0dd6fe1bae033
xuewc <wcxuemail@gmail.com>
2023-11-15 00:21:53 +0800

    update

--- docs/conf.py
+++ docs/conf.py
@@ -9,7 +9,9 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath('./../../bayespec'))
+print(sys.path[0])
 sys.path.insert(0, os.path.abspath('./../../'))
+print(sys.path[0])
 
 
 
@@ -42,5 +44,5 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 # import sphinx_rtd_theme
 html_theme = 'sphinx_rtd_theme'
-html_static_path = ['_static']
+# html_static_path = ['_static']
 # html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
