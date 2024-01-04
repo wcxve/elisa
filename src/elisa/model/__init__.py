20b44cf52a2b5b4b239fa8eee659f920b18aaada
S.-L. Xie <82627490+xiesl97@users.noreply.github.com>
2024-01-04 18:16:02 +0800

    Update __init__.py
    modify the file convol for onedrive sync

--- src/elisa/model/__init__.py
+++ src/elisa/model/__init__.py
@@ -2,6 +2,6 @@ from .base import *
 from .add import *
 from .mul import *
 from .ncon import *
-from .con import *
+from .convol import *
 
-__all__ = base.__all__ + add.__all__ + mul.__all__ + con.__all__ + ncon.__all__
+__all__ = base.__all__ + add.__all__ + mul.__all__ + convol.__all__ + ncon.__all__
