b4f2e4df779f4645ba3757f926eb3678f0aee059
xuewc <wcxuemail@gmail.com>
2024-01-09 22:16:53 +0800

    refactor: split __version__ from __init__.py
    Version information now stores in _version.py

--- src/elisa/__init__.py
+++ src/elisa/__init__.py
@@ -2,8 +2,7 @@ from .data import *
 from .model import *
 from .infer import *
 from .util import *
-
-__version__ = '0.0.1.dev1'
+from ._version import __version__
 
 jax_enable_x64(True)
 set_cpu_cores(4)
