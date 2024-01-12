52e57511695b6d4c6597bca528aeba183d2667ab
xuewc <wcxuemail@gmail.com>
2024-01-13 02:07:59 +0800

    update versioning

--- src/elisa/__init__.py
+++ src/elisa/__init__.py
@@ -2,7 +2,7 @@ from .data import *
 from .model import *
 from .infer import *
 from .util import *
-from ._version import __version__
+from .__about__ import __version__
 
 jax_enable_x64(True)
 set_cpu_cores(4)
