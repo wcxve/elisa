22c940e6d744d2db07991d5b4738994001bcfe92
xuewc <wcxuemail@gmail.com>
2024-01-15 05:12:07 +0800

    test: nox

--- noxfile.py
+++ noxfile.py
@@ -6,4 +6,4 @@ PYTHON_VERSIONS = ["3.9", "3.10", "3.11"]
 @nox.session(python=PYTHON_VERSIONS)
 def test(session: nox.Session) -> None:
     session.install(".[test]")
-    session.run("pytest", "--cov-report=xml", *session.posargs)
+    session.run("pytest", "--cov-report=xml", "--cov", *session.posargs)
