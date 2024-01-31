04b838cb88c24b1f279cb8c8717808114a3878d8
xuewc <wcxuemail@gmail.com>
2024-02-01 07:44:32 +0800

    chore: clean up

--- src/v0/__init__.py
+++
@@ -1,7 +0,0 @@
-from .data import Data
-from .inference import Infer
-from .model import *
-
-
-__all__ = ['Data', 'Infer']
-__all__.extend(model.__all__)
--- src/v0/model/__init__.py
+++
@@ -1,9 +0,0 @@
-from .base import UniformParameter
-from .flux_model import *
-from .model import *
-
-from .xspec import xs
-
-__all__ = ['UniformParameter', 'xs']
-__all__.extend(flux_model.__all__)
-__all__.extend(model.__all__)
