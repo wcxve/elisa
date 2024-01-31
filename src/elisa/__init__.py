94a29de5793fe009fde5cd63e74f7467fde97cde
xuewc <wcxuemail@gmail.com>
2024-02-01 06:56:59 +0800

    refactor: util routine

--- src/elisa/__init__.py
+++ src/elisa/__init__.py
@@ -1,8 +1,5 @@
-from .data import *
-from .model import *
-from .infer import *
-from .util import *
-from .__about__ import __version__
+from .model import *  # noqa: F403
+from .util import jax_enable_x64, set_cpu_cores
 
 jax_enable_x64(True)
 set_cpu_cores(4)
