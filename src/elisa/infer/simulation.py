e0f98f2a56bdf003eb97d4f73132eff206a3adbb
xuewc <wcxuemail@gmail.com>
2024-03-02 06:01:22 +0800

    minor update

--- src/elisa/infer/simulation.py
+++ src/elisa/infer/simulation.py
@@ -1,39 +1,40 @@
 """Data simulation and fit."""
 from __future__ import annotations
 
-from typing import Callable, Optional
+from typing import TYPE_CHECKING, Callable
 
 import jax
 import numpy as np
-
 from jax.experimental.mesh_utils import create_device_mesh
 from jax.sharding import PositionalSharding
 from numpyro import handlers
 
-from . import fit as _fit
 from .likelihood import PoissonWithGoodness
+
+if TYPE_CHECKING:
+    from .fit import BaseFit
 
 
 def _random_normal(seed, loc, scale, n=None):
-    np.random.default_rng(seed)
+    rng = np.random.default_rng(seed)
     if n is None:
         shape = loc.shape
     else:
         shape = (n,) + loc.shape
-    return np.random.normal(loc, scale, shape)
+    return rng.normal(loc, scale, shape)
 
 
 def _random_poisson(seed, lam, n=None):
-    np.random.default_rng(seed)
+    rng = np.random.default_rng(seed)
     if n is None:
         shape = lam.shape
     else:
         shape = (n,) + lam.shape
-    return np.random.poisson(lam, shape).astype(np.float64)
+    return rng.poisson(lam, shape).astype(np.float64)
 
 
 class Simulator:
-    """Observation simulator for numpyro model.
+    """Observation simulator for a numpyro model.
 
     This simulator uses :mod:`numpy.random` instead of :mod:`jax.random` for
     the performance.
@@ -51,16 +52,16 @@ class Simulator:
         self._model = model
         self._seed = int(seed)
         self._get_dist = jax.jit(self._make_dist_fn())
-        device = create_device_mesh((jax.device_count(),))
+        device = create_device_mesh((jax.local_device_count(),))
         self._sharding = PositionalSharding(device)
 
-    def sample_from_one_set(
+    def samples_from_one_set(
         self,
         params: dict[str, float],
         n: int,
-        seed: Optional[int] = None
+        seed: int | None = None,
     ) -> dict[str, np.ndarray]:
-        """Sample from one set of parameters.
+        """Samples from one set of parameters.
 
         Parameters
         ----------
@@ -74,7 +75,7 @@ class Simulator:
 
         Returns
         -------
-        sample : dict
+        samples : dict
             Simulated observation samples.
 
         """
@@ -85,39 +86,31 @@ class Simulator:
 
         dist = self._get_dist(params)
 
-        poisson_sample = jax.tree_map(
-            lambda v: _random_poisson(seed, v, n),
-            dist['poisson']
-        )
+        samples = {
+            k: _random_poisson(seed, *v, n) for k, v in dist['poisson'].items()
+        } | {k: _random_normal(seed, *v, n) for k, v in dist['normal'].items()}
 
-        normal_sample = jax.tree_map(
-            lambda v: _random_normal(seed, v[0], v[1], n),
-            dist['normal']
-        )
+        return samples
 
-        sample = poisson_sample | normal_sample
-
-        return sample
-
-    def sample_from_multi_sets(
+    def samples_from_multi_sets(
         self,
         params: dict[str, np.ndarray | jax.Array],
-        seed: Optional[int] = None
+        seed: int | None = None,
     ) -> dict[str, np.ndarray]:
-        """Sample from multiple sets of parameters.
+        """Samples from multiple sets of parameters.
 
         Parameters
         ----------
         params : dict
             Parameters in constrained space. Note that the parameter size must
-            be multiple of :func:`jax.device_count()`.
+            be multiple of :func:`jax.local_device_count()`.
         seed : int, optional
             Random number seed used in sampling. By default, use the seed given
             in the initialization.
 
         Returns
         -------
-        sample : dict
+        samples : dict
             Simulated observation samples.
 
         """
@@ -129,19 +122,11 @@ class Simulator:
         sharded_params = jax.device_put(params, self._sharding)
         dist = jax.vmap(self._get_dist)(sharded_params)
 
-        poisson_sample = jax.tree_map(
-            lambda v: _random_poisson(seed, v),
-            dist['poisson']
-        )
+        samples = {
+            k: _random_poisson(seed, *v) for k, v in dist['poisson'].items()
+        } | {k: _random_normal(seed, *v) for k, v in dist['normal'].items()}
 
-        normal_sample = jax.tree_map(
-            lambda v: _random_normal(seed, v[0], v[1]),
-            dist['normal']
-        )
-
-        sample = poisson_sample | normal_sample
-
-        return sample
+        return samples
 
     def _make_dist_fn(self) -> Callable:
         model = self._model
@@ -156,7 +141,7 @@ class Simulator:
 
             Returns
             -------
-            dist : dict
+            prior : dict
                 Sample distributions.
 
             """
@@ -166,20 +151,12 @@ class Simulator:
             dist = {'poisson': {}, 'normal': {}}
 
             for k, v in trace.items():
-                flag1 = k.endswith('_Non')
-                flag2 = k.endswith('_Noff')
-
-                if flag1 or flag2:
-                    if flag1:
-                        k2 = k[:-4] + '_spec'
-                    else:
-                        k2 = k[:-5] + '_back'
-
+                if k.endswith('_Non') or k.endswith('_Noff'):
                     fn = v['fn']
                     if isinstance(fn, PoissonWithGoodness):
-                        dist['poisson'][k2] = fn.rate
+                        dist['poisson'][f'{k}_data'] = (fn.rate,)
                     else:  # instance of NormalWithGoodness
-                        dist['normal'][k2] = (fn.loc, fn.scale)
+                        dist['normal'][f'{k}_data'] = (fn.loc, fn.scale)
 
             return dist
 
@@ -187,7 +164,7 @@ class Simulator:
 
 
 class SimFit:
-    def __init__(self, fit: _fit.BaseFit):
+    def __init__(self, fit: BaseFit):
         self._free_names = fit._free_names
         self._to_unconstr = fit._helper.to_unconstr_array
         self._net_counts = fit._helper.net_counts
@@ -201,9 +178,10 @@ class SimFit:
         self,
         params: dict[str, float],
         n: int,
-        seed: Optional[int] = None,
+        seed: int | None = None,
         parallel: bool = True,
-        run_str: Optional[str] = None
+        run_str: str | None = None,
+        progress: bool = True,
     ):
         """Simulate from one set of parameters and fit the simulation.
 
@@ -220,6 +198,8 @@ class SimFit:
             Whether to run the fit in parallel. The default is True.
         run_str : str, optional
             Description of the progress bar.
+        progress : bool, optional
+            Whether to display progress bar. The default is True.
 
         Returns
         -------
@@ -237,7 +217,7 @@ class SimFit:
         else:
             seed = int(seed)
 
-        sim_data = self._simulator.sample_from_one_set(params, n, seed)
+        sim_data = self._simulator.samples_from_one_set(params, n, seed)
         result = self._result_container(n)
         init_unconstr = self._to_unconstr(
             [params[i] for i in self._free_names]
@@ -249,16 +229,17 @@ class SimFit:
         else:
             fitter = self._sequence_fitter
 
-        fit_result = fitter(sim_data, result, init_unconstr, run_str)
+        fit_result = fitter(sim_data, result, init_unconstr, run_str, progress)
 
         return self._make_result(sim_data, fit_result)
 
     def run_multi_sets(
         self,
-        params: dict[str, jax.Array],
-        seed: Optional[int] = None,
+        params: dict[str, jax.Array | np.ndarray],
+        seed: int | None = None,
         parallel: bool = True,
-        run_str: Optional[str] = None
+        run_str: str | None = None,
+        progress: bool = True,
     ):
         """Simulate from multiple sets of parameters and fit the simulation.
 
@@ -273,6 +254,8 @@ class SimFit:
             Whether to run the fit in parallel. The default is True.
         run_str : str, optional
             Description of the progress bar.
+        progress : bool, optional
+            Whether to display progress bar. The default is True.
 
         Returns
         -------
@@ -290,7 +273,7 @@ class SimFit:
         else:
             seed = int(seed)
 
-        sim_data = self._simulator.sample_from_multi_sets(params, seed)
+        sim_data = self._simulator.samples_from_multi_sets(params, seed)
         init_unconstr = jax.vmap(self._to_unconstr)(
             np.column_stack([params[i] for i in self._free_names])
         )
@@ -301,10 +284,86 @@ class SimFit:
         else:
             fitter = self._sequence_fitter
 
-        fit_result = fitter(sim_data, result, init_unconstr, run_str)
+        fit_result = fitter(sim_data, result, init_unconstr, run_str, progress)
 
         return self._make_result(sim_data, fit_result)
 
-    def _make_result(self, sim_data, result_container):
-        result_container['data'] = sim_data | self._net_counts(sim_data)
-        return result_container
+    def fit_data(
+        self,
+        params: dict,
+        data: dict,
+        parallel: bool = True,
+        run_str: str | None = None,
+        progress: bool = True,
+    ):
+        """Fit the simulation data.
+
+        Parameters
+        ----------
+        params : dict
+            Parameters in constrained space.
+        data : dict
+            Simulation data based on `params`.
+        parallel : bool, optional
+            Whether to run the fit in parallel. The default is True.
+        run_str : str, optional
+            Description of the progress bar.
+        progress : bool, optional
+            Whether to display progress bar. The default is True.
+
+        Returns
+        -------
+        results : dict
+            The fit results.
+
+        """
+        p_in = set(params.keys())
+        p_all = set(self._free_names)
+        if p_in != p_all:
+            raise ValueError(f'require params {p_in - p_all}')
+
+        params = jax.tree_map(np.atleast_1d, params)
+        data = jax.tree_map(np.atleast_2d, data)
+
+        params_shape = list(jax.tree_map(np.shape, params).values)
+        data_shape = list(jax.tree_map(np.shape, data).values)
+
+        if any(i != params_shape[0] for i in params_shape):
+            raise ValueError('params shape must be all equal')
+
+        if any(i != data_shape[0] for i in data_shape):
+            raise ValueError('data shape must be all equal')
+
+        if params_shape[0] == 1:
+            init_unconstr = self._to_unconstr(
+                np.array(params[i] for i in self._free_names)
+            )
+            init_unconstr = np.full(
+                (data_shape[0], len(init_unconstr)), init_unconstr
+            )
+        else:
+            if params_shape[0] != data_shape[0]:
+                raise ValueError('params and data size must be equal')
+
+            init_unconstr = jax.vmap(self._to_unconstr)(
+                np.column_stack([params[i] for i in self._free_names])
+            )
+
+        result = self._result_container(data_shape[0])
+
+        if parallel:
+            fitter = self._parallel_fitter
+        else:
+            fitter = self._sequence_fitter
+
+        fit_result = fitter(data, result, init_unconstr, run_str, progress)
+
+        return self._make_result(data, fit_result)
+
+    def _make_result(self, sim_data, result):
+        result['data'] = sim_data | self._net_counts(sim_data)
+        valid = result.pop('valid')
+        result = jax.tree_map(lambda x: x[valid], result)
+        result['valid'] = valid
+        result['n_valid'] = int(valid.sum())
+        return result
