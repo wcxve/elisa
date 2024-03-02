f9cafe5f8ceb4e3a6766e2d718537af1cad30d91
xuewc <wcxuemail@gmail.com>
2024-03-03 05:16:05 +0800

    docs: update

--- src/elisa/__init__.py
+++ src/elisa/__init__.py
@@ -1,6 +1,6 @@
 from .__about__ import __version__ as __version__
-from .data.ogip import *  # noqa F403
-from .infer.fit import *  # noqa F403
+from .data import *  # noqa F403
+from .infer import *  # noqa F403
 from .model import *  # noqa F403
 from .util import jax_enable_x64, set_cpu_cores
 
