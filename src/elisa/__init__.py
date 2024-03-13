ca17c5414334f21908ff3a78e7cab7d4a8f02c3f
xuewc <wcxuemail@gmail.com>
2024-03-14 02:47:41 +0800

    better package init

--- src/elisa/__init__.py
+++ src/elisa/__init__.py
@@ -1,7 +1,20 @@
 from .__about__ import __version__ as __version__
-from .data import *  # noqa F403
-from .infer import *  # noqa F403
-from .model import *  # noqa F403
+from .data import Data as Data, Response as Response, Spectrum as Spectrum
+from .infer import BayesianFit as BayesianFit, MaxLikeFit as MaxLikeFit
+from .models.model import (
+    AnaIntAdditive as AnaIntAdditive,
+    AnaIntMultiplicative as AnaIntMultiplicative,
+    ConvolutionComponent as ConvolutionComponent,
+    NumIntAdditive as NumIntAdditive,
+    NumIntMultiplicative as NumIntMultiplicative,
+)
+from .models.parameter import (
+    CompositeParameter as CompositeParameter,
+    ConstantInterval as ConstantInterval,
+    ConstantValue as ConstantValue,
+    DistParameter as DistParameter,
+    UniformParameter as UniformParameter,
+)
 from .util import jax_enable_x64, set_cpu_cores
 
 jax_enable_x64(True)
