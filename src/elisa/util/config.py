84446ba85de1258959e4a1d31b399a0ab63646c8
S.-L. Xie <82627490+xiesl97@users.noreply.github.com>
2024-03-26 10:50:39 +0800

    Update config.py

--- src/elisa/util/config.py
+++ src/elisa/util/config.py
@@ -5,8 +5,9 @@ from __future__ import annotations
 import warnings
 from multiprocessing import cpu_count
 
+import os
+import jax
 from numpyro import enable_x64, set_host_device_count
-
 
 def jax_enable_x64(use_x64: bool) -> None:
     """Changes the default float precision of arrays in JAX.
@@ -16,11 +17,33 @@ def jax_enable_x64(use_x64: bool) -> None:
     use_x64 : bool
         When `True`, JAX arrays will use 64 bits else 32 bits.
     """
-    enable_x64(bool(use_x64))
+    if not use_x64:
+        use_x64 = os.getenv("JAX_ENABLE_X64", 0)
+    jax.config.update("jax_enable_x64", use_x64)
 
+def set_platform(platform: str="cpu"):
+    """
+    Changes platform to CPU, GPU, or TPU. This utility only takes
+    effect at the beginning of your program.
+
+    :param str platform: either 'cpu', 'gpu', or 'tpu'. The default is 'cpu'.
+    """
+
+    jax.config.update("jax_platform_name", platform)
+
+    # <https://jax.readthedocs.io/en/latest/gpu_performance_tips.html>
+    if platform == "gpu":
+        os.environ['XLA_FLAGS'] = (
+            '--xla_gpu_enable_triton_softmax_fusion=true '
+            '--xla_gpu_triton_gemm_any=True '
+            '--xla_gpu_enable_async_collectives=true '
+            '--xla_gpu_enable_latency_hiding_scheduler=true '
+            '--xla_gpu_enable_highest_priority_async_stream=true '
+        )
 
 def set_cpu_cores(n: int) -> None:
     """Set CPU number to use, should be called before running JAX codes.
+    This utility only takes effect at CPU platform.
 
     Parameters
     ----------
@@ -30,10 +53,25 @@ def set_cpu_cores(n: int) -> None:
     n = int(n)
     total_cores = cpu_count()
 
+    if jax.default_backend() != "cpu":
+        # raise RuntimeError("Please set default backend to 'cpu' before runing!")
+        set_platform()
+
     if n > total_cores:
         msg = f'only {total_cores} CPUs available, '
         msg += f'will use {total_cores - 1} CPUs'
         warnings.warn(msg, Warning)
         n = total_cores - 1
 
-    set_host_device_count(n)
+    os.environ["XLA_FLAGS"] = "--xla_force_host_platform_device_count={}".format(n)
+
+def set_debug_nan(flag: bool):
+    """Automatically detect when NaNs are produced.
+    <https://jax.readthedocs.io/en/latest/debugging/flags.html>
+
+    Parameters
+    ----------
+    flag : bool
+        When `True`, raises an error when NaNs is detected.
+    """
+    jax.config.update("jax_debug_nans", flag)
