906b654764475a8624ebc81b69fa79b0fd5bec1a
xuewc <wcxuemail@gmail.com>
2024-04-20 17:44:10 +0800

    feat: finite difference JVP rule with effective handling of fixed parameters

--- src/elisa/util/misc.py
+++ src/elisa/util/misc.py
@@ -9,8 +9,10 @@ from typing import TYPE_CHECKING
 
 import jax
 import jax.numpy as jnp
-from jax import lax
+from jax import lax, tree_util
+from jax.custom_derivatives import SymbolicZero
 from jax.experimental import host_callback
+from jax.flatten_util import ravel_pytree
 from prettytable import PrettyTable
 from tqdm.auto import tqdm
 
@@ -180,48 +182,63 @@ def define_fdjvp(
             f"'{method}'"
         )
 
-    use_central_diff = method == 'central'
-    fn = jax.custom_jvp(fn)
-
-    @fn.defjvp
-    def _(primals, tangents):
-        # we ignore the tangent for the energy grid
+    def fdjvp(primals, tangents):
         egrid, params = primals
+        egrid_tangent, params_tangent = tangents
+
+        if not isinstance(egrid_tangent, SymbolicZero):
+            raise NotImplementedError('JVP for energy grid is not implemented')
+
         primals_out = fn(egrid, params)
-        f_vmap = jax.vmap(fn, in_axes=(None, 0), out_axes=0)
-        eps = jnp.finfo(egrid.dtype).eps
-        identity = jnp.eye(len(params))
-        params_keys = list(params.keys())
-        params_values = jnp.array(list(params.values()))
-        params_abs = jnp.where(
-            jnp.equal(params_values, 0.0),
-            jnp.ones_like(params_values),
-            jnp.abs(params_values),
+
+        tvals, _ = tree_util.tree_flatten(params_tangent)
+        if any(jnp.shape(v) != () for v in tvals):
+            raise NotImplementedError(
+                'JVP for non-scalar parameter is not implemented'
+            )
+
+        params = lax.stop_gradient(params)
+        non_zero_tangents = [not isinstance(v, SymbolicZero) for v in tvals]
+        idx = [i for i, v in enumerate(non_zero_tangents) if v]
+        idx_arr = jnp.array(idx)
+        nbatch = sum(non_zero_tangents)
+        nparam = len(tvals)
+        params_ravel, revert = ravel_pytree(params)
+        free_params_values = params_ravel[idx_arr]
+        free_params_abs = jnp.where(
+            jnp.equal(free_params_values, 0.0),
+            jnp.ones_like(free_params_values),
+            jnp.abs(free_params_values),
         )
-        params_abs = jnp.expand_dims(params_abs, axis=-1)
+        free_params_abs = jnp.expand_dims(free_params_abs, axis=-1)
+        row_idx = jnp.arange(nbatch)
+        perturb_idx = jnp.zeros((nbatch, nparam)).at[row_idx, idx_arr].set(1.0)
+        params_batch = jnp.full((nbatch, nparam), params_ravel)
+
+        eps = jnp.finfo(egrid.dtype).eps
+        f_vmap = jax.vmap(fn, in_axes=(None, 0), out_axes=0)
+        revert = jax.vmap(revert, in_axes=0, out_axes=0)
 
         # See Numerical Recipes Chapter 5.7
-        if use_central_diff:
-            delta = params_abs * eps ** (1.0 / 3.0)
-            params_pos_perturb = jax.tree_map(
-                jnp.add, params, dict(zip(params_keys, delta * identity))
-            )
+        if method == 'central':
+            perturb = free_params_abs * eps ** (1.0 / 3.0)
+            params_pos_perturb = revert(params_batch + perturb_idx * perturb)
             out_pos_perturb = f_vmap(egrid, params_pos_perturb)
-            params_neg_perturb = jax.tree_map(
-                jnp.subtract, params, dict(zip(params_keys, delta * identity))
-            )
+            params_neg_perturb = revert(params_batch - perturb_idx * perturb)
             out_neg_perturb = f_vmap(egrid, params_neg_perturb)
-            d_out = (out_pos_perturb - out_neg_perturb) / (2.0 * delta)
+            d_out = (out_pos_perturb - out_neg_perturb) / (2.0 * perturb)
         else:
-            delta = params_abs * jnp.sqrt(eps)
-            params_perturb = jax.tree_map(
-                jnp.add, params, dict(zip(params_keys, delta * identity))
-            )
+            perturb = free_params_abs * jnp.sqrt(eps)
+            params_perturb = revert(params_batch + perturb_idx * perturb)
             out_perturb = f_vmap(egrid, params_perturb)
-            d_out = (out_perturb - primals_out) / delta
+            d_out = (out_perturb - primals_out) / perturb
 
-        tangents_out = jnp.array(list(tangents[1].values())) @ d_out
+        free_params_tangent = jnp.array([tvals[i] for i in idx])
+        tangents_out = free_params_tangent @ d_out
         return primals_out, tangents_out
+
+    fn = jax.custom_jvp(fn)
+    fn.defjvp(fdjvp, symbolic_zeros=True)
 
     return fn
 
