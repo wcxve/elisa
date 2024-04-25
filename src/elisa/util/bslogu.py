878c6369a58fdf8692829b4de51ea6d54ddbecd0
W.-C. Xue <58248583+wcxve@users.noreply.github.com>
2024-04-26 01:33:21 +0800

    refactor: bslogu.py

--- src/elisa/util/bslogu.py
+++ src/elisa/util/bslogu.py
@@ -1,4 +1,13 @@
+"""Bi-Symmetric log transformation for uniform distribution.
+
+See https://iopscience.iop.org/article/10.1088/0957-0233/24/2/027001.
+
+"""
+
+from __future__ import annotations
+
 import jax
+import jax.numpy as jnp
 from numpyro.distributions import Uniform, constraints
 from numpyro.distributions.distribution import (
     Transform,
@@ -7,16 +16,12 @@ from numpyro.distributions.distribution import (
 from numpyro.distributions.util import promote_shapes
 
 
-# # Bi-Symmetric log transformation
-# # https://iopscience.iop.org/article/10.1088/0957-0233/24/2/027001
 @jax.jit
 def log(x, c=0.43429448190325176):
     """
     transformation  x -> y
     c default to 1/log(10)
     """
-    import jax.numpy as jnp
-
     return jnp.sign(x) * jnp.log10(1 + jnp.abs(x / c))
 
 
@@ -26,17 +31,15 @@ def pow(y, c=0.43429448190325176):
     inverse transformation  y -> x
     c default to 1/log(10)
     """
-    import jax.numpy as jnp
-
     return jnp.sign(y) * c * (-1 + jnp.power(10, jnp.abs(y)))
 
 
 class BiSymTransform(Transform):
-    # TODO: refine domain/codomain Bi-Symmetric logic through setters, especially when
-    # transforms for inverses are supported
-    def __init__(self, domain=constraints.real):
+    # TODO: refine domain/codomain Bi-Symmetric logic through setters,
+    #  especially when transforms for inverses are supported
+    def __init__(self, domain=constraints.real, c=0.43429448190325176):
         self.domain = domain
-        self._c = 0.43429448190325176  # 1/log(10)
+        self._c = c
 
     @property
     def codomain(self):
@@ -64,23 +67,15 @@ class BiSymTransform(Transform):
         return log(y, self._c)
 
     def log_abs_det_jacobian(self, x, y, intermediates=None):
-        return x
+        return jnp.log(jnp.pow(10.0, jnp.abs(x)))
 
     def tree_flatten(self):
-        return (self.domain,), (('domain',), dict())
+        return (self.domain,), (('domain',), {})
 
     def __eq__(self, other):
         if not isinstance(other, BiSymTransform):
             return False
         return self.domain == other.domain
-
-    @property
-    def c(self):
-        return self._c
-
-    @c.setter
-    def c(self, value):
-        self._c = value
 
 
 class BiSymLogUniform(TransformedDistribution):
@@ -104,13 +99,20 @@ class BiSymLogUniform(TransformedDistribution):
     reparametrized_params = ['low', 'high']
     pytree_data_fields = ('low', 'high', '_support')
 
-    def __init__(self, low, high, *, validate_args=None):
-        self._c = 0.43429448190325176  # 1/log(10)
-        base_dist = Uniform(log(low, self._c), log(high, self._c))
+    def __init__(
+        self,
+        low,
+        high,
+        *,
+        c=0.43429448190325176,
+        validate_args=None,
+    ):
+        base_dist = Uniform(log(low, c), log(high, c))
+        self._c = c
         self.low, self.high = promote_shapes(low, high)
         self._support = constraints.interval(self.low, self.high)
         super(BiSymLogUniform, self).__init__(
-            base_dist, BiSymTransform(), validate_args=validate_args
+            base_dist, BiSymTransform(c=c), validate_args=validate_args
         )
 
     @constraints.dependent_property(is_discrete=False, event_dim=0)
@@ -119,27 +121,11 @@ class BiSymLogUniform(TransformedDistribution):
 
     @property
     def mean(self):
-        return (self.high - self.low) / log(self.high / self.low, self._c)
+        raise NotImplementedError
 
     @property
     def variance(self):
-        return (
-            0.5
-            * (self.high**2 - self.low**2)
-            / log(self.high / self.low, self._c)
-            - self.mean**2
-        )
+        raise NotImplementedError
 
     def cdf(self, x):
         return self.base_dist.cdf(log(x, self._c))
-
-    @property
-    def c(self):
-        return self._c
-
-    @c.setter
-    def c(self, value):
-        self._c = value
-        for s in self.transforms:
-            if hasattr(s, 'c'):
-                s.c = value
