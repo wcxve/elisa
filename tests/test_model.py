3b6668984993cf0bff6bbc234185ccf59cfb7f79
xuewc <wcxuemail@gmail.com>
2024-05-15 23:16:18 +0800

    test: add test for `CompiledModel.simulate`

--- tests/test_model.py
+++ tests/test_model.py
@@ -3,7 +3,7 @@ from astropy.cosmology import Planck18
 from astropy.units import Unit
 
 from elisa import ConstantValue
-from elisa.models import PowerLaw, ZAShift
+from elisa.models import PLPhFlux, PowerLaw, ZAShift
 
 
 def test_name():
@@ -60,3 +60,51 @@ def test_lumin_and_eiso():
 
     assert np.allclose(lumin, lumin_calc)
     assert np.allclose(eiso, eiso_calc)
+
+
+def test_simulation():
+    def powerlaw(alpha, K, egrid):
+        egrid = np.array(egrid)
+        one_minus_alpha = 1.0 - alpha
+        f = K / one_minus_alpha * np.power(egrid, one_minus_alpha)
+        return f[1:] - f[:-1]
+
+    seed = 42
+    emin = 1.0
+    emax = 100.0
+    nbins = 10
+    photon_egrid = np.linspace(emin, emax, nbins + 1)
+    channel_emin = photon_egrid[:-1]
+    channel_emax = photon_egrid[1:]
+    response_matrix = np.eye(nbins)
+    spec_exposure = 100.0
+    back_exposure = 50.0
+    background = np.ones(nbins) * 100.0
+
+    rng = np.random.default_rng(seed)
+    back_counts = rng.poisson(background)
+
+    alpha = 2.5
+    F = 1000.0  # photon flux between emin and emax
+    unscaled_flux = powerlaw(alpha, 1.0, photon_egrid)
+    factor = F / powerlaw(alpha, 1.0, [emin, emax])
+    source = unscaled_flux * factor * spec_exposure
+    total = source + spec_exposure * background / back_exposure
+    total_counts = rng.poisson(total)
+
+    model = PLPhFlux(emin, emax, F=F, alpha=alpha).compile()
+    data = model.simulate(
+        photon_egrid=photon_egrid,
+        channel_emin=channel_emin,
+        channel_emax=channel_emax,
+        response_matrix=response_matrix,
+        spec_exposure=spec_exposure,
+        spec_poisson=True,
+        back_counts=background,
+        back_exposure=back_exposure,
+        back_poisson=True,
+        seed=seed,
+    )
+
+    assert np.all(data.spec_counts == total_counts)
+    assert np.all(data.back_counts == back_counts)
