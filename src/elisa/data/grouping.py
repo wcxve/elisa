fix: add sign to Li-Ma and GV significance (#88)
* fix: add sign to Li-Ma significance and GV significance

* ylog settings of spectral data plot

* add test for grouping based on GV significance

src/elisa/data/grouping.py
@@ -257,8 +257,8 @@ def significance_lima(
         term2[mask] = xlogy(
             n_off[mask], (1.0 + a) * n_off[mask] / (n_on[mask] + n_off[mask])
         )
-
-    return np.sqrt(2.0 * (term1 + term2))
+    sign = np.where(n_on >= a * n_off, 1.0, -1.0)
+    return sign * np.sqrt(2.0 * (term1 + term2))
 
 
 def significance_gv(
@@ -288,7 +288,8 @@ def significance_gv(
     term1 += b0_mle - n
 
     term2 = np.square(b - b0_mle) / (2 * s2)
-    return np.sqrt(2.0 * (term1 + term2))
+    sign = np.where(n >= b, 1.0, -1.0)
+    return sign * np.sqrt(2.0 * (term1 + term2))
 
 
 def group_optsig_normal(

tests/test_data.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-from elisa.data.grouping import significance_lima
+from elisa.data.grouping import significance_gv, significance_lima
 from elisa.models import PowerLaw
 
 
@@ -115,4 +115,32 @@ def test_data_grouping():
     scale = 1
     data.group('optbsig', scale)
     sig = data.back_counts / data.back_errors
+    assert np.all(sig >= scale)
+
+    data = compiled_model.simulate(
+        photon_egrid=photon_egrid,
+        channel_emin=channel_emin,
+        channel_emax=channel_emax,
+        response_matrix=response_matrix,
+        spec_exposure=spec_exposure,
+        spec_poisson=True,
+        back_counts=np.full(nbins, 10),
+        back_errors=np.full(nbins, 2),
+        back_exposure=2.0,
+        back_poisson=False,
+        seed=seed,
+    )
+
+    scale = 1
+    data.group('sig', scale)
+    sig = significance_gv(
+        data.spec_counts, data.back_counts, data.back_errors, data.back_ratio
+    )
+    assert np.all(sig >= scale)
+
+    scale = 1
+    data.group('optbsig', scale)
+    sig = significance_gv(
+        data.spec_counts, data.back_counts, data.back_errors, data.back_ratio
+    )
     assert np.all(sig >= scale)