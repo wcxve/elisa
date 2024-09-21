1d8556d3357f3abfc820626ca9ef12ba356d8a7b
S.-L. Xie <82627490+xiesl97@users.noreply.github.com>
2024-09-22 02:21:39 +0800

    fix: scipy_nquad

--- src/elisa/util/scipy_nquad.py
+++ src/elisa/util/scipy_nquad.py
@@ -12,16 +12,14 @@ try:
     import numba as nb
 except ImportError as e:
     raise ImportError(
-        'To use this module, please install `numba` package. It can be'
-        ' installed with `pip install numba`'
+        "To use this module, please install `numba` package. It can be"
+        " installed with `pip install numba`"
     ) from e
 
 
 class NQuadTransform:
     def __init__(self, fun):
-        self._c_sig = nb.types.double(
-            nb.types.intc, nb.types.CPointer(nb.types.double)
-        )
+        self._c_sig = nb.types.double(nb.types.intc, nb.types.CPointer(nb.types.double))
         self._fun = fun
 
     def _cfun(self):
@@ -48,40 +46,15 @@ class NQuadTransform:
                 )
                 return jnp.asarray([result, abserr])
 
-            result_shape_dtype = jax.ShapeDtypeStruct(
-                shape=(2,), dtype=ranges.dtype
-            )
+            result_shape_dtype = jax.ShapeDtypeStruct(shape=(2,), dtype=ranges.dtype)
             return jax.pure_callback(
                 _pcb, result_shape_dtype, ranges, args, vectorized=vectorized
             )
 
         return _nquad_scipy
 
-    def _args_grad(self, h=1e-7):
-        cfun = self._cfun()
-        func_nquad = jax.jit(self._nquad(cfun))
 
-        @jax.custom_jvp
-        @jax.jit
-        def _fn(ranges, args):
-            return func_nquad(ranges, args)[0]
-
-        @_fn.defjvp
-        @jax.jit
-        def _fn_jvp(primals, tangents):
-            ranges, args = primals
-            ranges_dot, args_dots = tangents
-            primal_out = _fn(ranges, args)
-            args_h = args + jnp.eye(len(args)) * h
-            primal_dx = jax.vmap(_fn, in_axes=(None, 0))(ranges, args_h)
-            primal_grad_dx = (primal_dx - primal_out) / h
-            tangent_out = jnp.sum(primal_grad_dx * args_dots)
-            return primal_out, tangent_out
-
-        return _fn
-
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     """example 1"""
 
     @nb.njit
@@ -89,40 +62,61 @@ if __name__ == '__main__':
         x, y, z, d = params
         return np.exp(-(x**2)) + y + z * d
 
+    # integrate variables x and y
     ranges = jnp.asarray([[0.0, 1.0], [0.0, 1.0]], dtype=jnp.float64)
+    # pass constant to z and d
     args = jnp.asarray([3.0, 4.0], dtype=jnp.float64)
+    # transform the function
     nqt = NQuadTransform(f)
     cfun = nqt._cfun()
     func_nquad = jax.jit(nqt._nquad(cfun, opts=None, vectorized=False))
-    func_arg_grad = nqt._args_grad()
+    # print result and error
     print(func_nquad(ranges, args))
-    print(jax.grad(func_arg_grad)(ranges, args))
 
     """example 2"""
     from elisa.models.model import AnaIntAdditive, ParamConfig
     from elisa.util.typing import JAXArray, NameValMapping
+    from elisa.util.misc import define_fdjvp
 
+    # blackbody model
     @nb.njit
     def bbodyrad(params):
         e, kT, K = params
         return 1.0344e-3 * K * e * e / np.expm1(e / kT)
 
-    nqt = NQuadTransform(bbodyrad)
-    func_arg_grad = nqt._args_grad()
+    # transform the model
+    nqt_bbodyrad = NQuadTransform(bbodyrad)
+    nqt_bbodyrad_cfun = nqt_bbodyrad._cfun()
+    bbodyrad_nquad = jax.jit(
+        nqt_bbodyrad._nquad(nqt_bbodyrad_cfun, opts=None, vectorized=False)
+    )
 
-    class BlackbodyRad_test(AnaIntAdditive):
+    # test integrate
+    ranges = jnp.asarray([[0.0, 1.0]], dtype=jnp.float64)
+    args = jnp.asarray([2.0, 3.0], dtype=jnp.float64)
+    print(bbodyrad_nquad(ranges, args))
+
+    @jax.jit
+    def bboduyrad_flux(ranges, args):
+        return bbodyrad_nquad(ranges, args)[0]  # return result
+
+    class BB_test(AnaIntAdditive):
         _config = (
-            ParamConfig('kT', 'kT', 'keV', 3.0, 1e-4, 200.0),
-            ParamConfig('K', 'K', '', 1.0, 1e-10, 1e10),
+            ParamConfig("kT", "kT", "keV", 3.0, 1e-4, 200.0),
+            ParamConfig("K", "K", "", 1.0, 1e-10, 1e10),
         )
 
         @staticmethod
         def integral(egrid: JAXArray, params: NameValMapping) -> JAXArray:
-            kT = params['kT']
-            K = params['K']
+            kT = params["kT"]
+            K = params["K"]
 
+            # integrate energy grids
             ranges = jnp.asarray([egrid[:-1], egrid[1:]], dtype=jnp.float64).T
             ranges = jnp.reshape(ranges, (len(ranges), 1, 2))
             args = jnp.asarray([kT, K], dtype=jnp.float64)
 
-            return jax.vmap(func_arg_grad, in_axes=(0, None))(ranges, args)
+            return jax.vmap(bboduyrad_flux, in_axes=(0, None))(ranges, args)
+
+    # define numerical integration for model fit
+    BB_test.integral = define_fdjvp(BB_test.integral, method="forward")
