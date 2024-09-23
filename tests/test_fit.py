e8a7c69c7e0cbb5d136eb0c20a32509a076204de
S.-L. Xie <82627490+xiesl97@users.noreply.github.com>
2024-09-23 13:46:52 +0800

    feat: add sampler `ESS` and `SA` (#120)
    * add `SA` sampler
    
    * add `ESS` sampler
    
    * test: add tests for `ess`
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: W.-C. Xue <58248583+wcxve@users.noreply.github.com>

--- tests/test_fit.py
+++ tests/test_fit.py
@@ -103,6 +103,11 @@ def test_trivial_bayes_fit():
             'chain_method': 'parallel',
             'n_parallel': 4,
         },
+        'ess': {
+            'chain_method': 'parallel',
+            'n_parallel': 4,
+        },
+        # 'sa': {},
         'ultranest': {},
         'nautilus': {},
     }
@@ -110,6 +115,9 @@ def test_trivial_bayes_fit():
     for method, kwargs in sampling_kwargs.items():
         # Get Bayesian fit result, i.e. posterior
         result = getattr(BayesFit(data, model), method)(**kwargs)
+
+        # check convergence
+        assert all(i < 1.01 for i in result.rhat.values() if not np.isnan(i))
 
         # check the true parameters values are within the 90% CI
         ci = result.ci(cl=0.9).intervals
