7934e1274af518a625e2c499ec122530e3c67b71
W.C. Xue <58248583+wcxve@users.noreply.github.com>
2025-03-18 17:14:23 +0800

    Update results.py

--- src/elisa/infer/results.py
+++ src/elisa/infer/results.py
@@ -181,8 +181,8 @@ class FitResult(ABC):
         compress : {'gzip', 'bz2', 'lzma'}
             The compression algorithm to use.
         **kwargs : dict
-            Extra parameters passed to :`gzip.open`, `bz2.open`
-            or `lzma.open`.
+            Extra parameters passed to :py:func:`gzip.open`,
+            :py:func:`bz2.open`, or :py:func:`lzma.open`.
         """
         if compress == 'gzip':
             open_ = gzip.open
