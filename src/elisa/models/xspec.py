ef8fdf7ca29855194a2f9b3010349a5fc7400139
xuewc <wcxuemail@gmail.com>
2025-04-30 08:49:57 +0800

    Better model docstring

--- src/elisa/models/xspec.py
+++ src/elisa/models/xspec.py
@@ -545,10 +545,7 @@ def xspec_model_info():
         desc = desc.text.strip()
         murl = url.format(models[0].title())
         for m in models:
-            model_info[m] = {
-                'desc': desc,
-                'link': f'{murl}#{m}',
-            }
+            model_info[m] = {'desc': desc, 'link': f'{murl}#{m}'}
     return model_info
 
 
