1c1ca27169d3249ec6d913c840de6856eeea48ca
xuewc <wcxuemail@gmail.com>
2025-05-02 01:46:15 +0800

    test: bound of `UniformParameter`

--- tests/test_parameter.py
+++ tests/test_parameter.py
@@ -7,3 +7,21 @@ def test_param_name():
     b = a + a2
     assert str(b) == "a + a'"
     assert b.default == 2.0
+
+
+def test_param_bound():
+    p = UniformParameter('p', 0.0, 0.0, 1.0, fixed=True)
+    assert p.default == p.min
+    p.fixed = False
+    assert p.min < p.default < p.max
+
+    p = UniformParameter('p', 0.0, -1.0, 0.0, fixed=True)
+    assert p.default == p.min
+    p.fixed = False
+    assert p.min < p.default < p.max
+
+    p = UniformParameter('p', 0.0, 0.0, 1.0)
+    assert p.min < p.default < p.max
+
+    p = UniformParameter('p', 0.0, -1.0, 0.0)
+    assert p.min < p.default < p.max
