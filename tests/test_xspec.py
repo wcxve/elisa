59e7bdb0dbe16b0c1da6ed4af762fbfd4e8184a4
xuewc <wcxuemail@gmail.com>
2025-05-02 04:56:21 +0800

    Fix test

--- tests/test_xspec.py
+++ tests/test_xspec.py
@@ -1,97 +1,17 @@
 import os
 
-import jax.numpy as jnp
 import numpy as np
 import pytest
 
 from elisa.models import Constant, PowerLaw
 from elisa.models.xspec import XspecConvolution, generate_xspec_models
 
-egrid = jnp.linspace(0.1, 10, 101)
+egrid = np.arange(0.1, 10, 0.01)
 const = Constant()
 pl = PowerLaw()
-excluded = ['XSsmaug']
-# There are 74 models depend on Xspec model data
-excluded_when_no_xspec_data = [
-    'XSbapec',
-    'XSbcheb6',
-    'XSbcie',
-    'XSbcoolflow',
-    'XSbcph',
-    'XSbexpcheb6',
-    'XSbexriv',
-    'XSbvapec',
-    'XSbvcempow',
-    'XSbvcheb6',
-    'XSbvcie',
-    'XSbvcoolflow',
-    'XSbvcph',
-    'XSbvgadem',
-    'XSbvvapec',
-    'XSbvvcie',
-    'XSbvvgadem',
-    'XSbvvwdem',
-    'XSbwdem',
-    'XSc6mekl',
-    'XSc6pmekl',
-    'XSc6pvmkl',
-    'XSc6vmekl',
-    'XScempow',
-    'XScevmkl',
-    'XScflow',
-    'XScheb6',
-    'XScie',
-    'XScompth',
-    'XScoolflow',
-    'XScph',
-    'XSeqpair',
-    'XSeqtherm',
-    'XSexpcheb6',
-    'XSgadem',
-    'XSismdust',
-    'XSkdblur',
-    'XSkdblur2',
-    'XSkerrbb',
-    'XSkerrd',
-    'XSlaor',
-    'XSlaor2',
-    'XSmeka',
-    'XSmkcflow',
-    'XSnlapec',
-    'XSnsmaxg',
-    'XSnsx',
-    'XSpexriv',
-    'XSrefsch',
-    'XSrfxconv',
-    'XStapec',
-    'XSvapec',
-    'XSvcempow',
-    'XSvcheb6',
-    'XSvcie',
-    'XSvcoolflow',
-    'XSvcph',
-    'XSvexpcheb6',
-    'XSvgadem',
-    'XSvmcflow',
-    'XSvmeka',
-    'XSvtapec',
-    'XSvvapec',
-    'XSvvcie',
-    'XSvvgadem',
-    'XSvvtapec',
-    'XSvvwdem',
-    'XSvwdem',
-    'XSwdem',
-    'XSxilconv',
-    'XSxscat',
-    'XSzkerrbb',
-    'XSzpowerlw',
-    'XSzvgauss',
-]
 
 
-@pytest.fixture()
-def has_xspec_data():
+def get_test_models():
     HEADAS = os.environ.get('HEADAS', '')
 
     if not HEADAS:
@@ -99,23 +19,31 @@ def has_xspec_data():
 
     path = os.path.abspath(f'{HEADAS}/../spectral/modelData')
     path_alt = os.path.abspath(f'{HEADAS}/spectral/modelData')
+    has_xspec_data = os.path.exists(path) or os.path.exists(path_alt)
 
-    return os.path.exists(path) or os.path.exists(path_alt)
+    models = generate_xspec_models()
+    if not has_xspec_data:
+        models_to_test = [
+            'XSzpowerlw',
+            'XSzvgauss',
+            'XStbabs',
+            'XSclumin',
+            'XSpartcov',
+            'XSvmshift',
+            'XSzmshift',
+        ]
+        models = {i: models[i] for i in models_to_test}
+    else:
+        models.pop('XSsmaug')
+
+    return models
 
 
 @pytest.mark.parametrize(
     'model',
-    [pytest.param(v, id=k) for k, v in generate_xspec_models().items()],
+    [pytest.param(v, id=k) for k, v in get_test_models().items()],
 )
-def test_xspec_model_eval(model, has_xspec_data):
-    name = model.__name__
-
-    if name in excluded:
-        pytest.skip(f'skip {name} as it is excluded')
-
-    if (not has_xspec_data) and (name in excluded_when_no_xspec_data):
-        pytest.skip(f'skip {name} as Xspec model data is missing')
-
+def test_xspec_model_eval(model):
     if not issubclass(model, XspecConvolution):
         # Additive or multiplicative models
         assert np.all(np.isfinite(model().compile().eval(egrid)))
