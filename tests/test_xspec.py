34f2389be459be53c8ecbfcc5c0c4d1df334a63a
xuewc <wcxuemail@gmail.com>
2025-05-02 20:06:28 +0800

    Update test

--- tests/test_xspec.py
+++ tests/test_xspec.py
@@ -3,10 +3,9 @@ import os
 import numpy as np
 import pytest
 
-from elisa.models import Constant, PowerLaw
-from elisa.models.xspec import XspecConvolution, generate_xspec_models
+from elisa.models import Constant, PowerLaw, xspec as xs
 
-egrid = np.arange(0.1, 10, 0.01)
+egrid = np.linspace(0.1, 10, 100)
 const = Constant()
 pl = PowerLaw()
 
@@ -21,21 +20,27 @@ def get_test_models():
     path_alt = os.path.abspath(f'{HEADAS}/spectral/modelData')
     has_xspec_data = os.path.exists(path) or os.path.exists(path_alt)
 
-    models = generate_xspec_models()
+    models = xs.generate_xspec_models()
     if not has_xspec_data:
         models_to_test = [
-            'XSposm',
-            'XSzpowerlw',
-            'XSzvgauss',
-            'XStbabs',
-            'XSclumin',
-            'XSpartcov',
-            'XSvmshift',
-            'XSzmshift',
+            'XSfeklor',  # test for additive models with norm param only
+            'XSposm',  # test for additive models with norm param only
+            'XSzkerrbb',  # test for cosmology setting
+            'XStbabs',  # test multiplicative model
+            'XSclumin',  # test convolution model (for additive models)
+            'XSpartcov',  # test convolution model (for multiplicative models)
+            'XSvmshift',  # test convolution model (for multiplicative models)
+            'XSzmshift',  # test convolution model (for multiplicative models)
         ]
         models = {i: models[i] for i in models_to_test}
     else:
         models.pop('XSsmaug')
+
+        # Fix nei models for XSPEC v12.15
+        xspec_version = xs.version()
+        major, minor, _ = xspec_version.split('.')
+        if major == '12' and minor == '15':
+            xs.set_model_string('NEIVERS', '3.1.2')
 
     return models
 
@@ -45,7 +50,7 @@ def get_test_models():
     [pytest.param(v, id=k) for k, v in get_test_models().items()],
 )
 def test_xspec_model_eval(model):
-    if not issubclass(model, XspecConvolution):
+    if not issubclass(model, xs.XspecConvolution):
         # Additive or multiplicative models
         assert np.all(np.isfinite(model().compile().eval(egrid)))
     else:
