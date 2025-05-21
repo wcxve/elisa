ac9d47dbc4d551dd07f18e6a48ade4926ba931c8
W.C. Xue <58248583+wcxve@users.noreply.github.com>
2025-05-21 18:36:09 +0800

    Set randomize_split to True to improve mixing

--- src/elisa/infer/fit.py
+++ src/elisa/infer/fit.py
@@ -803,7 +803,7 @@ class BayesFit(Fit):
         # TODO: option to let sampler starting from MLE
         if issubclass(kernel, NumpyroEnsembleSampler):
             # set randomize_split to True to improve mixing
-            kernel_kwargs.setdefault('randomize_split', False)
+            kernel_kwargs.setdefault('randomize_split', True)
             init = self._check_init(init)
             init = self._helper.constr_dic_to_unconstr_arr(init)
             n_params = len(init)
