18add9977d9393389ea6f30b3d867b5c68e21aee
xiesl97 <stephenmeotor@foxmail.com>
2026-08-31 23:33:36 +0800

    Build documentation notebooks serially

--- docs/build.sh
+++ docs/build.sh
@@ -22,7 +22,7 @@ sphinx-apidoc \
 sphinx-build \
     --builder html \
     --doctree-dir _build/doctrees \
-    --jobs auto \
+    --jobs 1 \
     --define language=en \
     --fail-on-warning \
     --keep-going \
